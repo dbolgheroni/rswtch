676045c75369d50f024fb70d054405d0a7b61a31
Daniel Bolgheroni <dbolgheroni@gmail.com>
2016-11-22 18:01:24 -0200

    Of course this is not the right place for it.

--- rswtch.py
+++ rswtch.py
@@ -23,11 +23,6 @@
 # CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
 # OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-#
-# TODO:
-# . include README.md documentation
-# . include sample for rswtch.conf
-# . improve readability of commands in Sh
 
 import argparse
 import cmd
